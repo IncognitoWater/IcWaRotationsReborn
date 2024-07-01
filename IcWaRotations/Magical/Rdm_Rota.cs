e5a2534ed0113e3b1b15733c3aaa0c9ef9408dc6
IncognitoWater <florentvanmeir@gmail.com>
2024-07-01 22:56:11 +0200

    IcWa - Linked RS to local build of main RS to test around and created Base level 100 rotation for summoner CURRENTLY WORKING ROTATION = SMN ONLY

--- IcWaRotations/Magical/Rdm_Rota.cs
+++ IcWaRotations/Magical/Rdm_Rota.cs
@@ -1,164 +1,164 @@
-namespace IcWaRotations.Magical;
-
-[Rotation("IncognitoWater's RDM", CombatType.PvE, GameVersion = "6.38")]
-[SourceCode(Path = "main/IcWaRotations/Magical/Rdm_Rota.cs")]
-[Api(1)]
-public sealed class RdmRotation : RedMageRotation
-{
-
-	[RotationConfig(CombatType.PvE, Name = "Use Vercure for Dualcast when out of combat.")]
-	public bool UseVercure { get; set; } = false;
-	
-	[RotationConfig(CombatType.PvE, Name = "Use Acceleration On Cooldown to avoid overstack")]
-	public bool UseAcceleration { get; set; } = false;
-	
-	private bool CanStartMeleeCombo
-	{
-		get
-		{
-			if (Player.HasStatus(true, StatusID.Manafication, StatusID.Embolden) ||
-				BlackMana == 100 || WhiteMana == 100) return true;
-
-			if (BlackMana == WhiteMana) return false;
-
-			if (WhiteMana < BlackMana)
-			{
-				if (Player.HasStatus(true, StatusID.VerstoneReady)) return false;
-			}
-			else
-			{
-				if (Player.HasStatus(true, StatusID.VerfireReady)) return false;
-			}
-
-			if (Player.HasStatus(true, VercurePvE.Setting.StatusProvide)) return false;
-
-			//Waiting for embolden.
-			if (EmboldenPvE.EnoughLevel && EmboldenPvE.Cooldown.WillHaveOneChargeGCD(5)) return false;
-
-			return true;
-		}
-	}
-	
-
-	protected override IAction CountDownAction(float remainTime)
-	{
-		if (remainTime < VerthunderIiiPvE.Info.CastTime + CountDownAhead
-			&& VerthunderIiiPvE.CanUse(out var act, CanUseOption.SkipComboCheck)) return act;
-
-		//Remove Swift
-		StatusHelper.StatusOff(StatusID.Dualcast);
-		StatusHelper.StatusOff(StatusID.Acceleration);
-		StatusHelper.StatusOff(StatusID.Swiftcast);
-
-		return base.CountDownAction(remainTime);
-	}
-
-	protected override bool GeneralGCD(out IAction act)
-	{
-		act = null;
-		if (ManaStacks == 3) return false;
-
-		if (!VerthunderIiiPvE.CanUse(out _))
-		{
-			if (VerfirePvE.CanUse(out act)) return true;
-			if (VerstonePvE.CanUse(out act)) return true;
-		}
-
-		if (ScatterPvE.CanUse(out act)) return true;
-		if (WhiteMana < BlackMana)
-		{
-			if (VeraeroIiiPvE.CanUse(out act) && BlackMana - WhiteMana != 5) return true;
-			if (VeraeroPvE.CanUse(out act) && BlackMana - WhiteMana != 6) return true;
-		}
-
-		if (VerthunderIiiPvE.CanUse(out act)) return true;
-		if (VerthunderPvE.CanUse(out act)) return true;
-
-		if (JoltPvE.CanUse(out act)) return true;
-
-		if (UseVercure && NotInCombatDelay && VercurePvE.CanUse(out act)) return true;
-
-		return base.GeneralGCD(out act);
-	}
-
-	protected override bool EmergencyGCD(out IAction act)
-	{
-		if (ManaStacks == 3)
-		{
-			if (BlackMana > WhiteMana)
-			{
-				if (VerholyPvE.CanUse(out act)) return true;
-			}
-
-			if (VerflarePvE.CanUse(out act)) return true;
-		}
-
-		if (ResolutionPvE.CanUse(out act)) return true;
-		if (ScorchPvE.CanUse(out act)) return true;
-
-
-		if (IsLastGCD(true, MoulinetPvE) && MoulinetPvE.CanUse(out act)) return true;
-		if (ZwerchhauPvE.CanUse(out act)) return true;
-		if (RedoublementPvE.CanUse(out act)) return true;
-
-		if (!CanStartMeleeCombo) return false;
-
-		if (MoulinetPvE.CanUse(out act))
-		{
-			if (BlackMana >= 60 && WhiteMana >= 60) return true;
-		}
-		else
-		{
-			if (BlackMana >= 50 && WhiteMana >= 50 && RipostePvE.CanUse(out act)) return true;
-		}
-
-		if (ManaStacks > 0 && RipostePvE.CanUse(out act)) return true;
-
-		return base.EmergencyGCD(out act);
-	}
-
-	protected override bool EmergencyAbility(IAction nextGCD, out IAction act)
-	{
-		act = null;
-
-		if (IsBurst && UseBurstMedicine(out act)) return true;
-
-		if (CombatElapsedLess(4)) return false;
-		if (ScorchPvE.CanUse(out act)) return false;
-		if (ResolutionPvE.CanUse(out act)) return false;
-
-		if (UseAcceleration && AccelerationPvE.Cooldown.CurrentCharges == 2 &&
-			AccelerationPvE.CanUse(out act)) return true;
-
-		if (IsBurst && HasHostilesInRange && EmboldenPvE.CanUse(out act)) return true;
-
-		if (((Player.HasStatus(true, StatusID.Embolden) || IsLastAbility(ActionID.EmboldenPvE)) && ManaStacks == 0 && WhiteMana <= 50 && BlackMana <= 50) &&
-			ManaficationPvE.CanUse(out act)) return true;
-
-		return base.EmergencyAbility(nextGCD, out act);
-	}
-
-	protected override bool AttackAbility(IAction nextGCD,out IAction act)
-	{
-		//Swift
-		if (ManaStacks == 0 && (BlackMana < 50 || WhiteMana < 50)
-			&& (CombatElapsedLess(4) || !ManaficationPvE.EnoughLevel ||
-				!ManaficationPvE.Cooldown.WillHaveOneChargeGCD(0, 1)))
-		{
-			if (!Player.HasStatus(true, StatusID.VerfireReady, StatusID.VerstoneReady))
-			{
-				if (SwiftcastPvE.CanUse(out act)) return true;
-				if (InCombat && AccelerationPvE.CanUse(out act, CanUseOption.SkipComboCheck)) return true;
-			}
-		}
-
-		//Attack abilities.
-		if (ContreSixtePvE.CanUse(out act)) return true;
-		if (FlechePvE.CanUse(out act)) return true;
-
-		if (EngagementPvE.CanUse(out act, CanUseOption.SkipComboCheck)) return true;
-		if (CorpsacorpsPvE.CanUse(out act) && !IsMoving) return true;
-
-		return base.AttackAbility(nextGCD, out act);
-	}
-}
+// namespace IcWaRotations.Magical;
+//
+// [Rotation("IncognitoWater's RDM", CombatType.PvE, GameVersion = "6.38")]
+// [SourceCode(Path = "main/IcWaRotations/Magical/Rdm_Rota.cs")]
+// [Api(1)]
+// public sealed class RdmRotation : RedMageRotation
+// {
+//
+// 	[RotationConfig(CombatType.PvE, Name = "Use Vercure for Dualcast when out of combat.")]
+// 	public bool UseVercure { get; set; } = false;
+// 	
+// 	[RotationConfig(CombatType.PvE, Name = "Use Acceleration On Cooldown to avoid overstack")]
+// 	public bool UseAcceleration { get; set; } = false;
+// 	
+// 	private bool CanStartMeleeCombo
+// 	{
+// 		get
+// 		{
+// 			if (Player.HasStatus(true, StatusID.Manafication, StatusID.Embolden) ||
+// 				BlackMana == 100 || WhiteMana == 100) return true;
+//
+// 			if (BlackMana == WhiteMana) return false;
+//
+// 			if (WhiteMana < BlackMana)
+// 			{
+// 				if (Player.HasStatus(true, StatusID.VerstoneReady)) return false;
+// 			}
+// 			else
+// 			{
+// 				if (Player.HasStatus(true, StatusID.VerfireReady)) return false;
+// 			}
+//
+// 			if (Player.HasStatus(true, VercurePvE.Setting.StatusProvide)) return false;
+//
+// 			//Waiting for embolden.
+// 			if (EmboldenPvE.EnoughLevel && EmboldenPvE.Cooldown.WillHaveOneChargeGCD(5)) return false;
+//
+// 			return true;
+// 		}
+// 	}
+// 	
+//
+// 	protected override IAction CountDownAction(float remainTime)
+// 	{
+// 		if (remainTime < VerthunderIiiPvE.Info.CastTime + CountDownAhead
+// 			&& VerthunderIiiPvE.CanUse(out var act, CanUseOption.SkipComboCheck)) return act;
+//
+// 		//Remove Swift
+// 		StatusHelper.StatusOff(StatusID.Dualcast);
+// 		StatusHelper.StatusOff(StatusID.Acceleration);
+// 		StatusHelper.StatusOff(StatusID.Swiftcast);
+//
+// 		return base.CountDownAction(remainTime);
+// 	}
+//
+// 	protected override bool GeneralGCD(out IAction act)
+// 	{
+// 		act = null;
+// 		if (ManaStacks == 3) return false;
+//
+// 		if (!VerthunderIiiPvE.CanUse(out _))
+// 		{
+// 			if (VerfirePvE.CanUse(out act)) return true;
+// 			if (VerstonePvE.CanUse(out act)) return true;
+// 		}
+//
+// 		if (ScatterPvE.CanUse(out act)) return true;
+// 		if (WhiteMana < BlackMana)
+// 		{
+// 			if (VeraeroIiiPvE.CanUse(out act) && BlackMana - WhiteMana != 5) return true;
+// 			if (VeraeroPvE.CanUse(out act) && BlackMana - WhiteMana != 6) return true;
+// 		}
+//
+// 		if (VerthunderIiiPvE.CanUse(out act)) return true;
+// 		if (VerthunderPvE.CanUse(out act)) return true;
+//
+// 		if (JoltPvE.CanUse(out act)) return true;
+//
+// 		if (UseVercure && NotInCombatDelay && VercurePvE.CanUse(out act)) return true;
+//
+// 		return base.GeneralGCD(out act);
+// 	}
+//
+// 	protected override bool EmergencyGCD(out IAction act)
+// 	{
+// 		if (ManaStacks == 3)
+// 		{
+// 			if (BlackMana > WhiteMana)
+// 			{
+// 				if (VerholyPvE.CanUse(out act)) return true;
+// 			}
+//
+// 			if (VerflarePvE.CanUse(out act)) return true;
+// 		}
+//
+// 		if (ResolutionPvE.CanUse(out act)) return true;
+// 		if (ScorchPvE.CanUse(out act)) return true;
+//
+//
+// 		if (IsLastGCD(true, MoulinetPvE) && MoulinetPvE.CanUse(out act)) return true;
+// 		if (ZwerchhauPvE.CanUse(out act)) return true;
+// 		if (RedoublementPvE.CanUse(out act)) return true;
+//
+// 		if (!CanStartMeleeCombo) return false;
+//
+// 		if (MoulinetPvE.CanUse(out act))
+// 		{
+// 			if (BlackMana >= 60 && WhiteMana >= 60) return true;
+// 		}
+// 		else
+// 		{
+// 			if (BlackMana >= 50 && WhiteMana >= 50 && RipostePvE.CanUse(out act)) return true;
+// 		}
+//
+// 		if (ManaStacks > 0 && RipostePvE.CanUse(out act)) return true;
+//
+// 		return base.EmergencyGCD(out act);
+// 	}
+//
+// 	protected override bool EmergencyAbility(IAction nextGCD, out IAction act)
+// 	{
+// 		act = null;
+//
+// 		if (IsBurst && UseBurstMedicine(out act)) return true;
+//
+// 		if (CombatElapsedLess(4)) return false;
+// 		if (ScorchPvE.CanUse(out act)) return false;
+// 		if (ResolutionPvE.CanUse(out act)) return false;
+//
+// 		if (UseAcceleration && AccelerationPvE.Cooldown.CurrentCharges == 2 &&
+// 			AccelerationPvE.CanUse(out act)) return true;
+//
+// 		if (IsBurst && HasHostilesInRange && EmboldenPvE.CanUse(out act)) return true;
+//
+// 		if (((Player.HasStatus(true, StatusID.Embolden) || IsLastAbility(ActionID.EmboldenPvE)) && ManaStacks == 0 && WhiteMana <= 50 && BlackMana <= 50) &&
+// 			ManaficationPvE.CanUse(out act)) return true;
+//
+// 		return base.EmergencyAbility(nextGCD, out act);
+// 	}
+//
+// 	protected override bool AttackAbility(IAction nextGCD,out IAction act)
+// 	{
+// 		//Swift
+// 		if (ManaStacks == 0 && (BlackMana < 50 || WhiteMana < 50)
+// 			&& (CombatElapsedLess(4) || !ManaficationPvE.EnoughLevel ||
+// 				!ManaficationPvE.Cooldown.WillHaveOneChargeGCD(0, 1)))
+// 		{
+// 			if (!Player.HasStatus(true, StatusID.VerfireReady, StatusID.VerstoneReady))
+// 			{
+// 				if (SwiftcastPvE.CanUse(out act)) return true;
+// 				if (InCombat && AccelerationPvE.CanUse(out act, CanUseOption.SkipComboCheck)) return true;
+// 			}
+// 		}
+//
+// 		//Attack abilities.
+// 		if (ContreSixtePvE.CanUse(out act)) return true;
+// 		if (FlechePvE.CanUse(out act)) return true;
+//
+// 		if (EngagementPvE.CanUse(out act, CanUseOption.SkipComboCheck)) return true;
+// 		if (CorpsacorpsPvE.CanUse(out act) && !IsMoving) return true;
+//
+// 		return base.AttackAbility(nextGCD, out act);
+// 	}
+// }
--- IcWaRotations/Ranged/Mch_Rota.cs
+++ IcWaRotations/Ranged/Mch_Rota.cs
@@ -1,197 +1,197 @@
-﻿namespace DefaultRotations.Ranged;
-
-[Rotation("IncognitoWater's Delayed Tools", CombatType.Both, GameVersion = "6.38")]
-[SourceCode(Path = "main/IcWaRotations/Ranged/Mch_Rota.cs")]
-[Api(1)]
-public sealed class MchRotation : MachinistRotation
-{
-	private const ActionID MarksmansSpitePvPActionId = (ActionID)29415;
-	private const double LbInPvpThreshold = 30000;
-	private const float RestTime = 6f;
-
-	[RotationConfig(CombatType.PvE, Name = "Reassemble with ChainSaw")]
-	public bool UseMchReassemble { get; set; } = false;
-
-	[RotationConfig(CombatType.PvP, Name = "Use the LB in PvP when Target is killable by it")]
-	public bool UseLbInPvp { get; set; } = true;
-
-	[RotationConfig(CombatType.PvP, Name = "Turn on if you want to FORCE RS to use nothing while in guard in PvP")]
-	public bool GuardCancel { get; set; } = true;
-
-	private IBaseAction MarksmansSpitePvP = new BaseAction(MarksmansSpitePvPActionId);
-	
-
-
-	protected override IAction? CountDownAction(float remainTime)
-	{
-		if (remainTime < CountDownAhead)
-		{
-			if (AirAnchorPvE.CanUse(out var act1)) return act1;
-			if (!AirAnchorPvE.EnoughLevel && HotShotPvE.CanUse(out act1)) return act1;
-		}
-		if (remainTime < 2 && UseBurstMedicine(out var act)) return act;
-		if (remainTime < 5 && ReassemblePvE.CanUse(out act, CanUseOption.UsedUp | CanUseOption.SkipClippingCheck)) return act;
-		return base.CountDownAction(remainTime);
-	}
-
-
-	protected override bool GeneralGCD(out IAction? act)
-	{
-		act = null;
-		
-		//Overheated
-		if (AutoCrossbowPvE.CanUse(out act)) return true;
-		if (HeatBlastPvE.CanUse(out act)) return true;
-
-		//Long Cds
-		if (BioblasterPvE.CanUse(out act)) return true;
-		if (!SpreadShotPvE.CanUse(out _))
-		{
-			if (AirAnchorPvE.CanUse(out act)) return true;
-			if (!AirAnchorPvE.EnoughLevel && HotShotPvE.CanUse(out act)) return true;
-
-			if (DrillPvE.CanUse(out act)) return true;
-		}
-
-		if (!CombatElapsedLessGCD(4) && ChainSawPvE.CanUse(out act)) return true;
-
-		//Aoe
-		if (ChainSawPvE.CanUse(out act)) return true;
-		if (SpreadShotPvE.CanUse(out act)) return true;
-
-		//Single
-		if (CleanShotPvE.CanUse(out act)) return true;
-		if (SlugShotPvE.CanUse(out act)) return true;
-		if (SplitShotPvE.CanUse(out act)) return true;
-		
-		#region PvP
-		if (GuardCancel && Player.HasStatus(true, StatusID.Guard)) return base.GeneralGCD(out act);
-		if (LimitBreakLevel==1 && HostileTarget && UseLbInPvp && HostileTarget.CurrentHp < LbInPvpThreshold && MarksmansSpitePvP.CanUse(out act, CanUseOption.SkipClippingCheck)) return true;
-
-		if (!Player.HasStatus(true, StatusID.Overheated_3149))
-		{
-			if (Player.HasStatus(true, StatusID.DrillPrimed))
-			{
-				if (DrillPvP.CanUse(out act, CanUseOption.UsedUp)) return true;
-			}
-			else if (Player.HasStatus(true, StatusID.BioblasterPrimed) && HostileTarget && HostileTarget.DistanceToPlayer() < BioblasterPvP.Action.Range)
-			{
-				if (BioblasterPvP.CanUse(out act, CanUseOption.UsedUp | CanUseOption.SkipAoeCheck)) return true;
-			}
-			else if (Player.HasStatus(true, StatusID.AirAnchorPrimed))
-			{
-				if (AirAnchorPvP.CanUse(out act, CanUseOption.UsedUp)) return true;
-			}
-			else if (Player.HasStatus(true, StatusID.ChainSawPrimed))
-			{
-				if (ChainSawPvP.CanUse(out act, CanUseOption.UsedUp | CanUseOption.SkipAoeCheck)) return true;
-			}
-		}
-
-		if (ScattergunPvP.CanUse(out act)) return true;
-		if (BlastChargePvP.CanUse(out act, CanUseOption.SkipCastingCheck)) return true;
-		#endregion
-
-		return base.GeneralGCD(out act);
-	}
-
-	protected override bool EmergencyAbility(IAction nextGCD, out IAction? act)
-	{
-		if (IsBurst)
-		{
-			if (UseBurstMedicine(out act)) return true;
-		}
-
-		if (UseMchReassemble && ChainSawPvE.EnoughLevel && nextGCD.IsTheSameTo(true, ChainSawPvE))
-		{
-			if (ReassemblePvE.CanUse(out act, CanUseOption.UsedUp)) return true;
-		}
-		if (RicochetPvE.CanUse(out act)) return true;
-		if (GaussRoundPvE.CanUse(out act)) return true;
-
-		if (!DrillPvE.EnoughLevel && nextGCD.IsTheSameTo(true, CleanShotPvE)
-			|| nextGCD.IsTheSameTo(false, AirAnchorPvE, ChainSawPvE, DrillPvE))
-		{
-			if (ReassemblePvE.CanUse(out act, CanUseOption.UsedUp)) return true;
-		}
-		
-		#region PvP
-		act = null;
-
-		if (GuardCancel && Player.HasStatus(true, StatusID.Guard)) return base.EmergencyAbility(nextGCD, out act);
-
-		if (Player.HasStatus(true, StatusID.Overheated_3149) && WildfirePvP.CanUse(out act)) return true;
-
-		if ((nextGCD.IsTheSameTo(ActionID.DrillPvP) || nextGCD.IsTheSameTo(ActionID.BioblasterPvP) && NumberOfHostilesInRange > 2 || nextGCD.IsTheSameTo(ActionID.AirAnchorPvP)) &&
-			!(IsLastAction(ActionID.DrillPvP) || IsLastAction(ActionID.BioblasterPvP) || IsLastAction(ActionID.AirAnchorPvP)) && AnalysisPvP.CanUse(out act)) return true;
-
-		if (BishopAutoturretPvP.CanUse(out act)) return true;
-		#endregion
-		
-		return base.EmergencyAbility(nextGCD, out act);
-	}
-
-	protected override bool AttackAbility(IAction nextGCD, out IAction? act)
-	{
-		if (IsBurst)
-		{
-			if ((IsLastAbility(false, HyperchargePvE) || Heat >= 50) && !CombatElapsedLess(10)
-				&& WildfirePvE.CanUse(out act, CanUseOption.OnLastAbility)) return true;
-		}
-
-		if (!CombatElapsedLess(12) && CanUseHypercharge(out act)) return true;
-		if (CanUseRookAutoturret(out act)) return true;
-
-		if (BarrelStabilizerPvE.CanUse(out act)) return true;
-
-		if (CombatElapsedLess(8)) return false;
-
-		var option = CanUseOption.UsedUp | CanUseOption.SkipComboCheck;
-		if (GaussRoundPvE.Cooldown.CurrentCharges <= RicochetPvE.Cooldown.CurrentCharges)
-		{
-			if (RicochetPvE.CanUse(out act, option)) return true;
-		}
-		if (GaussRoundPvE.CanUse(out act, option)) return true;
-
-		return base.AttackAbility(nextGCD, out act);
-	}
-
-
-	private bool CanUseRookAutoturret(out IAction act)
-	{
-		act = null;
-		if (AirAnchorPvE.EnoughLevel)
-		{
-			if (!AirAnchorPvE.IsInCooldown || AirAnchorPvE.Cooldown.ElapsedAfter(18)) return false;
-		}
-		else
-		{
-			if (!HotShotPvE.IsInCooldown || HotShotPvE.Cooldown.ElapsedAfter(18)) return false;
-		}
-
-		return RookAutoturretPvE.CanUse(out act);
-	}
-	
-	private bool CanUseHypercharge(out IAction act)
-	{
-		act = null;
-
-
-		//Check recast.
-		if (!SpreadShotPvE.CanUse(out _))
-		{
-			if (AirAnchorPvE.EnoughLevel)
-			{
-				if (AirAnchorPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
-			}
-			else
-			{
-				if (HotShotPvE.EnoughLevel && HotShotPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
-			}
-		}
-		if (DrillPvE.EnoughLevel && DrillPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
-		if (ChainSawPvE.EnoughLevel && ChainSawPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
-
-		return HyperchargePvE.CanUse(out act);
-	}
-}
+﻿// namespace DefaultRotations.Ranged;
+//
+// [Rotation("IncognitoWater's Delayed Tools", CombatType.Both, GameVersion = "6.38")]
+// [SourceCode(Path = "main/IcWaRotations/Ranged/Mch_Rota.cs")]
+// [Api(1)]
+// public sealed class MchRotation : MachinistRotation
+// {
+// 	private const ActionID MarksmansSpitePvPActionId = (ActionID)29415;
+// 	private const double LbInPvpThreshold = 30000;
+// 	private const float RestTime = 6f;
+//
+// 	[RotationConfig(CombatType.PvE, Name = "Reassemble with ChainSaw")]
+// 	public bool UseMchReassemble { get; set; } = false;
+//
+// 	[RotationConfig(CombatType.PvP, Name = "Use the LB in PvP when Target is killable by it")]
+// 	public bool UseLbInPvp { get; set; } = true;
+//
+// 	[RotationConfig(CombatType.PvP, Name = "Turn on if you want to FORCE RS to use nothing while in guard in PvP")]
+// 	public bool GuardCancel { get; set; } = true;
+//
+// 	private IBaseAction MarksmansSpitePvP = new BaseAction(MarksmansSpitePvPActionId);
+// 	
+//
+//
+// 	protected override IAction? CountDownAction(float remainTime)
+// 	{
+// 		if (remainTime < CountDownAhead)
+// 		{
+// 			if (AirAnchorPvE.CanUse(out var act1)) return act1;
+// 			if (!AirAnchorPvE.EnoughLevel && HotShotPvE.CanUse(out act1)) return act1;
+// 		}
+// 		if (remainTime < 2 && UseBurstMedicine(out var act)) return act;
+// 		if (remainTime < 5 && ReassemblePvE.CanUse(out act, CanUseOption.UsedUp | CanUseOption.SkipClippingCheck)) return act;
+// 		return base.CountDownAction(remainTime);
+// 	}
+//
+//
+// 	protected override bool GeneralGCD(out IAction? act)
+// 	{
+// 		act = null;
+// 		
+// 		//Overheated
+// 		if (AutoCrossbowPvE.CanUse(out act)) return true;
+// 		if (HeatBlastPvE.CanUse(out act)) return true;
+//
+// 		//Long Cds
+// 		if (BioblasterPvE.CanUse(out act)) return true;
+// 		if (!SpreadShotPvE.CanUse(out _))
+// 		{
+// 			if (AirAnchorPvE.CanUse(out act)) return true;
+// 			if (!AirAnchorPvE.EnoughLevel && HotShotPvE.CanUse(out act)) return true;
+//
+// 			if (DrillPvE.CanUse(out act)) return true;
+// 		}
+//
+// 		if (!CombatElapsedLessGCD(4) && ChainSawPvE.CanUse(out act)) return true;
+//
+// 		//Aoe
+// 		if (ChainSawPvE.CanUse(out act)) return true;
+// 		if (SpreadShotPvE.CanUse(out act)) return true;
+//
+// 		//Single
+// 		if (CleanShotPvE.CanUse(out act)) return true;
+// 		if (SlugShotPvE.CanUse(out act)) return true;
+// 		if (SplitShotPvE.CanUse(out act)) return true;
+// 		
+// 		#region PvP
+// 		if (GuardCancel && Player.HasStatus(true, StatusID.Guard)) return base.GeneralGCD(out act);
+// 		if (LimitBreakLevel==1 && HostileTarget && UseLbInPvp && HostileTarget.CurrentHp < LbInPvpThreshold && MarksmansSpitePvP.CanUse(out act, CanUseOption.SkipClippingCheck)) return true;
+//
+// 		if (!Player.HasStatus(true, StatusID.Overheated_3149))
+// 		{
+// 			if (Player.HasStatus(true, StatusID.DrillPrimed))
+// 			{
+// 				if (DrillPvP.CanUse(out act, CanUseOption.UsedUp)) return true;
+// 			}
+// 			else if (Player.HasStatus(true, StatusID.BioblasterPrimed) && HostileTarget && HostileTarget.DistanceToPlayer() < BioblasterPvP.Action.Range)
+// 			{
+// 				if (BioblasterPvP.CanUse(out act, CanUseOption.UsedUp | CanUseOption.SkipAoeCheck)) return true;
+// 			}
+// 			else if (Player.HasStatus(true, StatusID.AirAnchorPrimed))
+// 			{
+// 				if (AirAnchorPvP.CanUse(out act, CanUseOption.UsedUp)) return true;
+// 			}
+// 			else if (Player.HasStatus(true, StatusID.ChainSawPrimed))
+// 			{
+// 				if (ChainSawPvP.CanUse(out act, CanUseOption.UsedUp | CanUseOption.SkipAoeCheck)) return true;
+// 			}
+// 		}
+//
+// 		if (ScattergunPvP.CanUse(out act)) return true;
+// 		if (BlastChargePvP.CanUse(out act, CanUseOption.SkipCastingCheck)) return true;
+// 		#endregion
+//
+// 		return base.GeneralGCD(out act);
+// 	}
+//
+// 	protected override bool EmergencyAbility(IAction nextGCD, out IAction? act)
+// 	{
+// 		if (IsBurst)
+// 		{
+// 			if (UseBurstMedicine(out act)) return true;
+// 		}
+//
+// 		if (UseMchReassemble && ChainSawPvE.EnoughLevel && nextGCD.IsTheSameTo(true, ChainSawPvE))
+// 		{
+// 			if (ReassemblePvE.CanUse(out act, CanUseOption.UsedUp)) return true;
+// 		}
+// 		if (RicochetPvE.CanUse(out act)) return true;
+// 		if (GaussRoundPvE.CanUse(out act)) return true;
+//
+// 		if (!DrillPvE.EnoughLevel && nextGCD.IsTheSameTo(true, CleanShotPvE)
+// 			|| nextGCD.IsTheSameTo(false, AirAnchorPvE, ChainSawPvE, DrillPvE))
+// 		{
+// 			if (ReassemblePvE.CanUse(out act, CanUseOption.UsedUp)) return true;
+// 		}
+// 		
+// 		#region PvP
+// 		act = null;
+//
+// 		if (GuardCancel && Player.HasStatus(true, StatusID.Guard)) return base.EmergencyAbility(nextGCD, out act);
+//
+// 		if (Player.HasStatus(true, StatusID.Overheated_3149) && WildfirePvP.CanUse(out act)) return true;
+//
+// 		if ((nextGCD.IsTheSameTo(ActionID.DrillPvP) || nextGCD.IsTheSameTo(ActionID.BioblasterPvP) && NumberOfHostilesInRange > 2 || nextGCD.IsTheSameTo(ActionID.AirAnchorPvP)) &&
+// 			!(IsLastAction(ActionID.DrillPvP) || IsLastAction(ActionID.BioblasterPvP) || IsLastAction(ActionID.AirAnchorPvP)) && AnalysisPvP.CanUse(out act)) return true;
+//
+// 		if (BishopAutoturretPvP.CanUse(out act)) return true;
+// 		#endregion
+// 		
+// 		return base.EmergencyAbility(nextGCD, out act);
+// 	}
+//
+// 	protected override bool AttackAbility(IAction nextGCD, out IAction? act)
+// 	{
+// 		if (IsBurst)
+// 		{
+// 			if ((IsLastAbility(false, HyperchargePvE) || Heat >= 50) && !CombatElapsedLess(10)
+// 				&& WildfirePvE.CanUse(out act, CanUseOption.OnLastAbility)) return true;
+// 		}
+//
+// 		if (!CombatElapsedLess(12) && CanUseHypercharge(out act)) return true;
+// 		if (CanUseRookAutoturret(out act)) return true;
+//
+// 		if (BarrelStabilizerPvE.CanUse(out act)) return true;
+//
+// 		if (CombatElapsedLess(8)) return false;
+//
+// 		var option = CanUseOption.UsedUp | CanUseOption.SkipComboCheck;
+// 		if (GaussRoundPvE.Cooldown.CurrentCharges <= RicochetPvE.Cooldown.CurrentCharges)
+// 		{
+// 			if (RicochetPvE.CanUse(out act, option)) return true;
+// 		}
+// 		if (GaussRoundPvE.CanUse(out act, option)) return true;
+//
+// 		return base.AttackAbility(nextGCD, out act);
+// 	}
+//
+//
+// 	private bool CanUseRookAutoturret(out IAction act)
+// 	{
+// 		act = null;
+// 		if (AirAnchorPvE.EnoughLevel)
+// 		{
+// 			if (!AirAnchorPvE.IsInCooldown || AirAnchorPvE.Cooldown.ElapsedAfter(18)) return false;
+// 		}
+// 		else
+// 		{
+// 			if (!HotShotPvE.IsInCooldown || HotShotPvE.Cooldown.ElapsedAfter(18)) return false;
+// 		}
+//
+// 		return RookAutoturretPvE.CanUse(out act);
+// 	}
+// 	
+// 	private bool CanUseHypercharge(out IAction act)
+// 	{
+// 		act = null;
+//
+//
+// 		//Check recast.
+// 		if (!SpreadShotPvE.CanUse(out _))
+// 		{
+// 			if (AirAnchorPvE.EnoughLevel)
+// 			{
+// 				if (AirAnchorPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
+// 			}
+// 			else
+// 			{
+// 				if (HotShotPvE.EnoughLevel && HotShotPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
+// 			}
+// 		}
+// 		if (DrillPvE.EnoughLevel && DrillPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
+// 		if (ChainSawPvE.EnoughLevel && ChainSawPvE.Cooldown.WillHaveOneCharge(RestTime)) return false;
+//
+// 		return HyperchargePvE.CanUse(out act);
+// 	}
+// }
